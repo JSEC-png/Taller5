4cfd4ef153ba6d5fc704a1d23d3ae913808d960b
Sebastian <sebastian.rivera.leon2000@gmail.com>
2020-07-15 09:53:18 -0500

    Pruebas de validación apellido

--- js/codigo.js
+++ js/codigo.js
@@ -1,25 +1,34 @@
-window.onload = function()
-{
-    var a = document.getElementById('prueba');
-    var b = document.getElementById('pruebahijo');
-    function ocultar()
-    {
-        b.style.display = 'none';                                
-    }
-    
+var nombre, apellido, direccion, usuario, pass, email, tel,label2;
+    nombre=document.getElementById("prueba").value;
+    direccion=document.getElementById("direccion").value;
+    usuario=document.getElementById("usuario").value;
+    pass=document.getElementById("pass").value;
+    email=document.getElementById("email").value;
+    tel=document.getElementById("tel").value;
 
-    function mostrar()
-    {
-        b.style.display = 'block';                                
-    }
+function validar2(){
+    apellido=document.getElementById("apellido").value;
+    label2=document.getElementById("label2");
 
-    a.onmouseover = function(){
-        b.style.display = 'none';                                
-    }
+        if (apellido.length > 25){
+            label2.innerHTML='Deben ser menos de 25 carácteres';
+            label2.style.display= 'block';
+        }else{
+            label2.style.display= 'none';
+        };
+  
+}
 
-    a.onmouseout = function(){
-        b.style.display = 'block';                                
+function validar(){
+    apellido=document.getElementById("apellido").value;
+    label2=document.getElementById("label2");
+    if (apellido==""){
+        label2.innerHTML='¡Campo vacío!';
+        label2.style.display='block';
     }
+    else{
+        label2.style.display='none';
+    };
 
 }
 
